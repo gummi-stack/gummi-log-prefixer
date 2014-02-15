4c65fe4c7ad016b80dd17f78a5f63ff5e8659417
Martin Cizek <martin.cizek@socialbakers.com>
2014-02-15 19:51:18 +0100

    Select & syslog

--- gummi-prefixer.c
+++ gummi-prefixer.c
@@ -1,54 +1,69 @@
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <syslog.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/select.h>
 
-static int *pids;
 
-static int BUFFER_SIZE = 262144;
+#define BUFFER_SIZE 262144
+
+char writeBuffer[BUFFER_SIZE];
 
 
 int readFromPipe(int fd, char *buffer, char *prefix) {
 	int rv;
+	memset(writeBuffer, '\0', BUFFER_SIZE);
 
+	if ( (rv = read(fd, buffer, BUFFER_SIZE)) < 0 ) {
+		printf("READ ERROR FROM PIPE");
+		return -1;
+	} else if (rv == 0) {
+		return -1;
+	}
 
-    if ( (rv = read(fd, buffer, BUFFER_SIZE)) < 0 ) {
-        printf("READ ERROR FROM PIPE");
-		return 0;
-    }
-    else if (rv == 0) {
-        return 0;
-    }
-
+	int writeLen = 0;
 	int dumpPrefix = 1;
 	size_t len = strlen(buffer);
 
 	for(int i = 0; i < len; i++) {
 		char c = buffer[i];
 		if(dumpPrefix == 1) {
-			printf("%s ", prefix);
+			memcpy(writeBuffer, prefix, strlen(prefix));
+
+			writeLen = strlen(writeBuffer);
+			writeBuffer[writeLen] = ' ';
+			writeLen++;
+
 			dumpPrefix = 0;
 		}
 		if (c == '\n') {
 			dumpPrefix = 1;
 		}
 
-		printf("%c", c);
+		writeBuffer[writeLen] = c;
+		writeLen++;
 	}
+	fprintf(stdout, "%s", writeBuffer);
+	syslog (LOG_INFO, writeBuffer);
 
-	return 1;
+	return 0;
 }
 
+// if more than 10, fix all string allocations (currently it expects only one digit number)
 int OUT_COUNT = 10;
 
 int main (int argc, char **argv) {
-	if(argc < 4) {
-		printf(" usage: %s 550e8400-e29b-41d4-a716-446655440000 parser.1 /path/to/mrdka arg1 arg2\n", argv[0]);
+	if(argc < 5) {
+		printf(" usage: %s syslogTag appUuid appName.orderNum command arg1 arg2\n", argv[0]);
 		return 1;
 	}
 
+	// syslog
+	setlogmask (LOG_UPTO (LOG_INFO));
+	openlog (argv[1], LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
 	int pipes[OUT_COUNT][2];
 
@@ -56,29 +71,24 @@ int main (int argc, char **argv) {
 		pipe(pipes[i]);
 	}
 
-	if (fork() == 0) {
-		char *params[argc];
-		memcpy(params, argv + 3, sizeof params);
-		params[argc-1] = NULL;
 
-		// dup2(pipefd[1], 1);  // send stdout to the pipe
-		// dup2(pipefd2[1], 2);  // send stderr to the pipe
+	if (fork() == 0) {
 		for(int i=0; i < OUT_COUNT; i++) {
 			dup2(pipes[i][1], i + 1);  // send stderr to the pipe
 			close(pipes[i][1]);
 		}
 
-		execvp(params[0], params);
+		execvp(argv[4], argv + 4);
 		return 0;
-
 	}
 
-	for(int i=0; i < OUT_COUNT; i++) {
-		int r = isatty(i+1);
+	int opened[OUT_COUNT];
+	int openedCount = 0;
+	char *prefixes[OUT_COUNT];
+	struct timeval tv;
+	int fdMax;
+	fd_set readfds;
 
-	}
-
-	int opened[OUT_COUNT]; // = malloc(sizeof(int)*OUT_COUNT);
 
 	for(int i=0; i < OUT_COUNT; i++) {
 		close(pipes[i][1]);  // close the write end of the pipe in the parent
@@ -86,41 +96,40 @@ int main (int argc, char **argv) {
 
 		if(isatty(i+1) == 1) {
 			opened[i] = 1;
+			openedCount++;
 		}
 
+		prefixes[i] = malloc(strlen(argv[2]) + strlen(argv[3]) + 4);
+		sprintf(prefixes[i], "%s %s %i", argv[2], argv[3], i + 1);
 	}
 
-
-	char *prefixes[OUT_COUNT];
-	for(int i = 0;i < OUT_COUNT; i++ ) {
-		prefixes[i] = malloc(strlen(argv[1]) + strlen(argv[2]) + 4);
-		sprintf(prefixes[i], "%s %s %i", argv[1], argv[2], i);
-	}
-
-
-
-
-	char **mat = (char **)malloc(OUT_COUNT * sizeof(char*));
-	for(int i = 0; i < OUT_COUNT; i++) {
-		mat[i] = (char *)malloc(BUFFER_SIZE * sizeof(char));
-	}
-
-
-
-
-	int cont = 1;
-	while (cont) {
+	int numActive = 0;
+	char mat[OUT_COUNT][BUFFER_SIZE];
+	while(openedCount > 0) {
+		fdMax = 0;
+		FD_ZERO(&readfds);
 		for(int i=0; i < OUT_COUNT; i++) {
-			if(opened[i] == 1) {
-				opened[i] = readFromPipe(pipes[i][0], mat[i], prefixes[i]);
+			FD_SET(pipes[i][0], &readfds);
+			if (pipes[i][0] > fdMax) {
+				fdMax = pipes[i][0];
 			}
 		}
-		cont = 0;
-		for(int i=0; i < OUT_COUNT; i++) {
-			cont = cont || opened[i];
+
+		tv.tv_sec = 2;
+		tv.tv_usec = 0;
+
+		if ((numActive = select(fdMax + 1, &readfds, NULL, NULL, &tv)) == -1) {
+			perror("select");
+			exit(1);
+		} else if (numActive == 0) {
+			printf("select() timed out\n");
+			continue;
 		}
-		// printf("cxcx %i", cont);
-		// opened[1] = readFromPipe(pipes[1][0], mat[1], prefix1);
-		// opened[2] = readFromPipe(pipes[2][0], mat[2], prefix1);
+
+		for(int i=0; i < OUT_COUNT; i++) {
+			if(FD_ISSET(pipes[i][0], &readfds)) {
+				openedCount += readFromPipe(pipes[i][0], mat[i], prefixes[i]);
+			}
+		}
 	}
 }
